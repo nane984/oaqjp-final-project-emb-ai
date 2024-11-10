40744decf2eeb264f04f7ae9bbc37854ed4eb680
nane984 <br.scekic@gmail.com>
2024-11-09 20:55:52 -0500

    static code analyses

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Module providing a function emotion detect."""
 from flask import Flask, render_template, request
 from emotion_detection.emotion_detection import emotion_detector
 
@@ -5,25 +6,30 @@ app = Flask("Emotion detection")
 
 @app.route("/")
 def render_index_page():
+    """Function rendering index.html"""
     return render_template('index.html')
 
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """Function analyze emotion"""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
+    all_results = [anger, disgust, fear, joy, sadness, dominant_emotion]
 
     if anger is None:
         return "Invalid text! Please try again!."
-    else:
-        return "For the given statement, the system response is 'anger': {},'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    res1 = "For the given statement, the system response is "
+    res2 = f"'anger': { all_results[0]},'disgust': { all_results[1]},"
+    res3 = f"'fear': { all_results[2]}, 'joy': { all_results[3]}"
+    res4 = f"and 'sadness': { all_results[4]}. The dominant emotion is { all_results[5]}"
+    return res1 + res2 + res3 + res4
 
 
 if __name__ == "__main__":
